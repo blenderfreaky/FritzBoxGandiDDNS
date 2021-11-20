09c0f25b4ea24351569a546fc68ef066aa4a35b8
blenderfreaky <blendcrafterxiv@gmail.com>
2021-11-20 12:18:34 +0100

    Minor Adjustments

--- FritzBoxGandiDDNS/Program.cs
+++ FritzBoxGandiDDNS/Program.cs
@@ -1,32 +1,37 @@
-﻿using System.Net.Http.Headers;
+﻿using Microsoft.Extensions.Primitives;
 
 var app = WebApplication.Create(args);
 var domainInfo = app.Configuration.GetSection("GandiDomain");
 
-string domainName = domainInfo.GetValue<string>("DomainName");
-string apiKey = domainInfo.GetValue<string>("ApiKey");
-string recordName = domainInfo.GetValue<string>("RecordName") ?? "@";
+string domainName = "", apiKey = "", recordName = "@";
+
+Action updateConfig = () =>
+{
+    domainName = domainInfo.GetValue<string>("DomainName");
+    apiKey = domainInfo.GetValue<string>("ApiKey");
+    recordName = domainInfo.GetValue<string>("RecordName") ?? "@";
+};
+updateConfig();
+
+ChangeToken.OnChange(() => app.Configuration.GetReloadToken(), updateConfig);
 
 using HttpClient http = new();
 http.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
 
 app.MapGet("/update", async (string? ipv4, string? ipv6) =>
 {
-    async Task<string> Put(string type, string address) =>
-        await (await http.PutAsJsonAsync(
-            $"https://dns.api.gandi.net/api/v5/domains/{domainName}/records/{recordName}/A",
-            new GandiAUpdateRequest(type, 300, address)))
-        .Content.ReadAsStringAsync();
-
     var now = $"{DateTime.Now:yyyy-MM-ddTHH\\:mm\\:ss}";
-    if (ipv4 != null)
+    async Task Put(string name, string type, string address)
     {
-        Console.WriteLine($"[{now}] Tried updating IPv4 A record {recordName}.{domainName} to {ipv4} (Response: \"{await Put("A", ipv4)}\")");
+        var response = await (await http.PutAsJsonAsync(
+                $"https://dns.api.gandi.net/api/v5/domains/{domainName}/records/{recordName}/{type}",
+                new GandiAUpdateRequest(type, 300, address)))
+            .Content.ReadAsStringAsync();
+        Console.WriteLine($"[{now}] Tried updating {name} {type} record {recordName}.{domainName} to {ipv4} (Response: \"{response}\")");
     }
-    if (ipv6 != null)
-    {
-        Console.WriteLine($"[{now}] Tried updating IPv6 AAAA record {recordName}.{domainName} to {ipv6} (Response: \"{await Put("AAAA", ipv6)}\")");
-    }
+
+    if (ipv4 != null) await Put("IPv4", "A", ipv4);
+    if (ipv6 != null) await Put("IPv6", "AAAA", ipv6);
 });
 
 await app.RunAsync();
