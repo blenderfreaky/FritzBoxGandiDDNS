e1e230f6eab3d89acbf24cbc43fb2b5f11ccffb6
blenderfreaky <blendcrafterxiv@gmail.com>
2021-11-20 11:06:18 +0100

    IPv6

--- FritzBoxGandiDDNS/Program.cs
+++ FritzBoxGandiDDNS/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System.Net.Http.Headers;
 
-//var config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
 var app = WebApplication.Create(args);
 var domainInfo = app.Configuration.GetSection("GandiDomain");
 
 string domainName = domainInfo.GetValue<string>("DomainName");
 string apiKey = domainInfo.GetValue<string>("ApiKey");
 string recordName = domainInfo.GetValue<string>("RecordName") ?? "@";
-string recordType = domainInfo.GetValue<string>("RecordType") ?? "A";
 
 using HttpClient http = new();
-http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("X-Api-Key", apiKey);
+http.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
 
-app.MapGet("/update", async (string ipv4) =>
+app.MapGet("/update", async (string? ipv4, string? ipv6) =>
 {
-    GandiAUpdateRequest request = new();
-    var response = await (
-        await http.PutAsJsonAsync($"https://dns.api.gandi.net/api/v5/domains/{domainName}/records/{recordName}/{recordType}", request))
+    async Task<string> Put(string type, string address) =>
+        await (await http.PutAsJsonAsync(
+            $"https://dns.api.gandi.net/api/v5/domains/{domainName}/records/{recordName}/A",
+            new GandiAUpdateRequest(type, 300, address)))
         .Content.ReadAsStringAsync();
 
-    Console.WriteLine($"[{DateTime.Now:yyyy-MM-ddTHH\\:mm\\:ss}] {(response == "DNS Record Created" ? "Successfully updated" : "Failed to update")} IP record to {ipv4}");
+    var now = $"{DateTime.Now:yyyy-MM-ddTHH\\:mm\\:ss}";
+    if (ipv4 != null)
+    {
+        Console.WriteLine($"[{now}] Tried updating IPv4 A record {recordName}.{domainName} to {ipv4} (Response: \"{await Put("A", ipv4)}\")");
+    }
+    if (ipv6 != null)
+    {
+        Console.WriteLine($"[{now}] Tried updating IPv6 AAAA record {recordName}.{domainName} to {ipv6} (Response: \"{await Put("AAAA", ipv6)}\")");
+    }
 });
 
 await app.RunAsync();
